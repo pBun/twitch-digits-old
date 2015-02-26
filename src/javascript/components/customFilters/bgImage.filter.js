e8f6b0669d8054573e6089ad6d07c4a223b919e8
Dustin Boersma <dustin.boersma@gmail.com>
2015-02-26 15:40:29 -0800

    added bgImage filter to prevent 404s when image url is null

---
+++ src/javascript/components/customFilters/bgImage.filter.js
@@ -0,0 +1,10 @@
+var angular = require('angular');
+
+var filter = function() {
+    return function (input) {
+        return input ? 'background-image: url(' + input + ');' : '';
+    }
+};
+filter.$inject = [];
+
+module.exports = filter;
