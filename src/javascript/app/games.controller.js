8f4b3c0f505007adf59d4458cdfec1752dbeb338
Dustin Boersma <dustin.boersma@gmail.com>
2015-02-28 03:56:10 -0800

    broke out build chart internal functions

--- src/javascript/app/games.controller.js
+++ src/javascript/app/games.controller.js
@@ -143,9 +143,13 @@ appController.prototype.buildChart = function(chartData) {
 
   chart.path.enter()
       .append('svg:path')
-      .on('mouseover', mouseover.bind(this))
-      .on('click', click.bind(this))
-      .each(stash);
+      .on('mouseover', this.mouseoverHandler.bind(this))
+      .on('click', this.clickHandler.bind(this))
+      .each(function(d) {
+        // Setup for switching data: stash the old values for transition.
+        d.x0 = d.x;
+        d.dx0 = d.dx;
+      });
 
   chart.path
     .attr('display', function(d) { return d.depth ? null : 'none'; })
@@ -155,132 +159,138 @@ appController.prototype.buildChart = function(chartData) {
     .style('fill', function(d) { return chart.colors(d.name); });
 
   // Add the mouseleave handler to the bounding circle.
-  chart.vis.on('mouseleave', mouseleave);
+  chart.vis.on('mouseleave', this.mouseleaveHandler);
 
   chart.viewers = chart.path.node().__data__.value;
 
-  // Fade all but the current sequence
-  function mouseover(d) {
+};
 
-    // Update current chart data
-    scope.chart.current = d;
-    scope.chart.currentGame = (d.type === 'stream') ? d.parent : d;
+// Fade all but the current sequence
+appController.prototype.mouseoverHandler = function(d) {
+
+  var chart = this.chart;
+  var scope = this.scope;
+
+  // Update current chart data
+  scope.chart.current = d;
+  scope.chart.currentGame = (d.type === 'stream') ? d.parent : d;
+  scope.$apply();
+
+  // Then highlight only those that are an ancestor of the current segment.
+  var sequenceArray = this.getAncestors(d);
+  chart.visWrapper
+    .classed('active', true)
+    .selectAll('path')
+      .classed('current', false)
+      .filter(function(d) {
+        var isDirectlyActive = sequenceArray.indexOf(d) >= 0;
+        var isChildOfActive = sequenceArray.indexOf(d.parent) >= 0;
+        var isActive = d.type === 'stream' ? isDirectlyActive : (isDirectlyActive || isChildOfActive);
+        return isActive;
+      })
+      .classed('current', true);
+}
+
+// Restore everything to full opacity when moving off the visualization.
+appController.prototype.mouseleaveHandler = function(d) {
+
+  var chart = this.chart;
+  var scope = this.scope;
+
+  // If we are zoomed, default to current game
+  if (chart.zoomed) {
+    chart.current = chart.currentGame;
     scope.$apply();
-
-    // Then highlight only those that are an ancestor of the current segment.
-    var sequenceArray = getAncestors(d);
-    chart.visWrapper
-      .classed('active', true)
-      .selectAll('path')
-        .classed('current', false)
-        .filter(function(node) {
-          var isDirectlyActive = sequenceArray.indexOf(node) >= 0;
-          var isChildOfActive = sequenceArray.indexOf(node.parent) >= 0;
-          var isActive = d.type === 'stream' ? isDirectlyActive : (isDirectlyActive || isChildOfActive);
-          return isActive;
-        })
-        .classed('current', true);
+    return;
   }
 
-  // Restore everything to full opacity when moving off the visualization.
-  function mouseleave(d) {
+  // Unset current chart data
+  chart.current = null;
+  chart.currentGame = null;
+  scope.$apply();
 
-    // If we are zoomed, default to current game
-    if (chart.zoomed) {
-      chart.current = chart.currentGame;
-      scope.$apply();
+  // Transition each segment to full opacity and then reactivate it.
+  chart.visWrapper
+    .classed('active', false)
+    .selectAll('path')
+      .classed('current', false);
+}
+
+appController.prototype.clickHandler = function(d) {
+
+  var chart = this.chart;
+  var scope = this.scope;
+
+  var mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  var isMobile = mobileRegex.test(navigator.userAgent);
+  if (d.type === 'stream') {
+    if (isMobile) {
       return;
     }
+    window.open(d.url);
+    return;
+  }
 
-    // Unset current chart data
-    chart.current = null;
-    chart.currentGame = null;
-    scope.$apply();
-
-    // Transition each segment to full opacity and then reactivate it.
+  chart.root = chart.root === d && d.parent ? d.parent : d;
+  var zooming = chart.root.type != 'root';
+  clearTimeout(this.chart.zoomTimeout);
+  chart.zoomed = zooming;
+  scope.$apply();
+  this.chart.zoomTimeout = setTimeout(function() {
     chart.visWrapper
-      .classed('active', false)
-      .selectAll('path')
-        .classed('current', false);
+      .classed('zoomed', zooming);
+  }, zooming ? 0 : 800);
+  chart.path.transition()
+    .duration(1000)
+    .attrTween("d", this.arcTweenZoom(chart.root));
+}
+
+// Given a node in a partition layout, return an array of all of its ancestor
+// nodes, highest first, but excluding the root.
+appController.prototype.getAncestors = function(node) {
+  var path = [];
+  var current = node;
+  while (current.parent) {
+    path.unshift(current);
+    current = current.parent;
   }
+  return path;
+}
 
-  function click(d) {
-    var mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-    var isMobile = mobileRegex.test(navigator.userAgent);
-    if (d.type === 'stream') {
-      if (isMobile) {
-        return;
-      }
-      window.open(d.url);
-      return;
-    }
-
-    chart.root = chart.root === d && d.parent ? d.parent : d;
-    var zooming = chart.root.type != 'root';
-    clearTimeout(this.chart.zoomTimeout);
-    chart.zoomed = zooming;
-    scope.$apply();
-    this.chart.zoomTimeout = setTimeout(function() {
-      chart.visWrapper
-        .classed('zoomed', zooming);
-    }, zooming ? 0 : 800);
-    chart.path.transition()
-      .duration(1000)
-      .attrTween("d", arcTweenZoom(chart.root));
+// When switching data: interpolate the arcs in data space.
+appController.prototype.arcTweenData = function(a, i) {
+  var chart = this.chart;
+  var oi = d3.interpolate({x: a.x0, dx: a.dx0}, a);
+  function tween(t) {
+    var b = oi(t);
+    a.x0 = b.x;
+    a.dx0 = b.dx;
+    return chart.arc(b);
   }
-
-  // Given a node in a partition layout, return an array of all of its ancestor
-  // nodes, highest first, but excluding the root.
-  function getAncestors(node) {
-    var path = [];
-    var current = node;
-    while (current.parent) {
-      path.unshift(current);
-      current = current.parent;
-    }
-    return path;
-  }
-
-  // Setup for switching data: stash the old values for transition.
-  function stash(d) {
-    d.x0 = d.x;
-    d.dx0 = d.dx;
-  }
-
-  // When switching data: interpolate the arcs in data space.
-  function arcTweenData(a, i) {
-    var oi = d3.interpolate({x: a.x0, dx: a.dx0}, a);
-    function tween(t) {
-      var b = oi(t);
-      a.x0 = b.x;
-      a.dx0 = b.dx;
-      return chart.arc(b);
-    }
-    if (i == 0) {
-     // If we are on the first arc, adjust the x domain to match the root node
-     // at the current zoom level. (We only need to do this once.)
-      var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]);
-      return function(t) {
-        chart.x.domain(xd(t));
-        return tween(t);
-      };
-    } else {
-      return tween;
-    }
-  }
-
-  // When zooming: interpolate the scales.
-  function arcTweenZoom(d) {
-    var xd = d3.interpolate(chart.x.domain(), [d.x, d.x + d.dx]),
-        yd = d3.interpolate(chart.y.domain(), [d.y, 1])
-        // yr = d3.interpolate(chart.y.range(), [d.y ? 20 : 0, chart.radius]);
-    return function(d, i) {
-      return i
-          ? function(t) { return chart.arc(d); }
-          : function(t) { chart.x.domain(xd(t)); chart.y.domain(yd(t)); return chart.arc(d); };
+  if (i == 0) {
+   // If we are on the first arc, adjust the x domain to match the root node
+   // at the current zoom level. (We only need to do this once.)
+    var xd = d3.interpolate(chart.x.domain(), [chart.root.x, chart.root.x + chart.root.dx]);
+    return function(t) {
+      chart.x.domain(xd(t));
+      return tween(t);
     };
+  } else {
+    return tween;
   }
+}
 
+// When zooming: interpolate the scales.
+appController.prototype.arcTweenZoom = function(d) {
+  var chart = this.chart;
+  var xd = d3.interpolate(chart.x.domain(), [d.x, d.x + d.dx]),
+      yd = d3.interpolate(chart.y.domain(), [d.y, 1])
+      // yr = d3.interpolate(chart.y.range(), [d.y ? 20 : 0, chart.radius]);
+  return function(d, i) {
+    return i
+        ? function(t) { return chart.arc(d); }
+        : function(t) { chart.x.domain(xd(t)); chart.y.domain(yd(t)); return chart.arc(d); };
+  };
 };
 
 
