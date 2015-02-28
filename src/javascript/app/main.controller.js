9963309777fc8e395b21008dc000813a11007784
Dustin Boersma <dustin.boersma@gmail.com>
2015-02-28 11:52:41 -0800

    friendlier control panel

--- src/javascript/app/main.controller.js
+++ src/javascript/app/main.controller.js
@@ -11,9 +11,18 @@ var appController = function($scope) {
   this.scope.chart.efficiency = 0.1;
   this.scope.chart.refresh = false;
   this.scope.chart.ready;
+  this.scope.calcRequests = this.calcRequests.bind(this);
 
 };
 appController.$inject = ['$scope'];
+
+appController.prototype.calcRequests = function() {
+    var chart = this.scope.chart;
+    var rootViewerReqs = (chart.gameLimit || chart.streamLimit) ? 1 : 0;
+    var gameReqs = chart.gameLimit < 100 ? 1 : Math.ceil(chart.gameLimit / 100);
+    var streamReqs = chart.gameLimit;
+    return rootViewerReqs + gameReqs + streamReqs;
+};
 
 
 module.exports = appController;
