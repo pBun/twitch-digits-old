cd63bcd93330a7cb6f9ea11b2152bc482ce8c2c5
Dustin Boersma <dustin.boersma@gmail.com>
2015-02-28 03:58:23 -0800

    added missing bind to fix mouseover bug

--- src/javascript/app/games.controller.js
+++ src/javascript/app/games.controller.js
@@ -159,7 +159,7 @@ appController.prototype.buildChart = function(chartData) {
     .style('fill', function(d) { return chart.colors(d.name); });
 
   // Add the mouseleave handler to the bounding circle.
-  chart.vis.on('mouseleave', this.mouseleaveHandler);
+  chart.vis.on('mouseleave', this.mouseleaveHandler.bind(this));
 
   chart.viewers = chart.path.node().__data__.value;
 
