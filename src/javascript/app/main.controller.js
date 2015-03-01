7cb9c34ffaf38918c439e0d477839fcacece97df
Dustin Boersma <dustin.boersma@gmail.com>
2015-02-28 16:12:12 -0800

    better req estimates and smaller default data set

--- src/javascript/app/main.controller.js
+++ src/javascript/app/main.controller.js
@@ -6,8 +6,8 @@ var appController = function($scope) {
 
   this.scope.chart = {};
   this.scope.chart.gameOffset = 0;
-  this.scope.chart.gameLimit = 25;
-  this.scope.chart.streamLimit = 0;
+  this.scope.chart.gameLimit = 50;
+  this.scope.chart.streamLimit = 100;
   this.scope.chart.efficiency = 0.1;
   this.scope.chart.refresh = false;
   this.scope.chart.error = '';
@@ -19,9 +19,18 @@ appController.$inject = ['$scope'];
 
 appController.prototype.calcRequests = function() {
     var chart = this.scope.chart;
+
+    // if we tally manually we don't request
     var rootViewerReqs = (chart.gameLimit || chart.streamLimit) ? 1 : 0;
-    var gameReqs = chart.gameLimit < 100 ? 1 : Math.ceil(chart.gameLimit / 100);
-    var streamReqs = chart.gameLimit;
+
+    // estimating ~1000 games
+    var gameReqs = chart.gameLimit <= 0 ? 1000 :
+      chart.gameLimit < 100 ? 1 : Math.ceil(chart.gameLimit / 100);
+
+    // estimating each game to contain ~500 streams
+    var streamReqs = chart.streamLimit <= 0 ? chart.gameLimit * 5 :
+      chart.gameLimit;
+
     return rootViewerReqs + gameReqs + streamReqs;
 };
 
