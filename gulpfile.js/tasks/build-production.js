7a676d57721b4a76afd87516c5f34ff3f675c208
Dustin Boersma <dustin.boersma@gmail.com>
2015-05-04 16:03:17 -0700

    added CNAME to production path

--- gulpfile.js/tasks/build-production.js
+++ gulpfile.js/tasks/build-production.js
@@ -1,7 +1,12 @@
 var gulp         = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
+gulp.task('copyCNAME', function() {
+  return gulp.src('./CNAME')
+    .pipe(gulp.dest('./public'));
+});
+
 gulp.task('build:production', function(cb) {
   process.env.NODE_ENV = 'production'
-  gulpSequence('clean', ['fonts', 'iconFont', 'images'], ['stylus', 'webpack:production'], 'html', 'rev', cb);
+  gulpSequence('clean', ['fonts', 'iconFont', 'images'], ['stylus', 'webpack:production'], 'html', 'rev', 'copyCNAME', cb);
 });
