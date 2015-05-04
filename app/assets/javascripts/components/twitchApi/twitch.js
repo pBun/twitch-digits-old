562e46b9adc86bcb681fb1cbb41ad07f3bf020f5
Dustin Boersma <dustin.boersma@gmail.com>
2015-05-04 14:03:05 -0700

    Update 2015-05-04T21:03:03.770Z

--- app/assets/javascripts/components/twitchApi/twitch.js
+++
@@ -1,8 +0,0 @@
-var angular = require('angular');
-
-var TwitchService = require('./twitch.service');
-
-var twitchModule = angular.module('TwitchModule', []);
-twitchModule.service('Twitch', TwitchService);
-
-module.exports = twitchModule;
--- gulpfile.js/config/deploy.js
+++
@@ -1,6 +0,0 @@
-var config = require('./')
-
-module.exports = {
-  url: 'http://pBun.github.io/twitch-digits/',
-  src: config.publicDirectory + '/**/*'
-}
